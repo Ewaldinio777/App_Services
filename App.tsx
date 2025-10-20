a584bb43334509e518e0cee866e4a67b849510ed
Ewaldinio777 <Edu.ve1524@gmail.com>
2025-10-20 13:19:54 -0400

    Creating the folders and put in the correct place

--- App.tsx
+++ App.tsx
@@ -1,14 +1,14 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 
-import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
+import { GluestackUIProvider } from "@/src/components/ui/gluestack-ui-provider";
 import "@/global.css";
 
 export default function App() {
   return (
     <GluestackUIProvider mode="dark">
       <View style={styles.container}>
-        <Text>Sexitoo</Text>
+        <Text>Sexitoo 123</Text>
         <StatusBar style="auto" />
       </View>
     </GluestackUIProvider>
